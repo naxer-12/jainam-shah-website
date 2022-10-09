7277cdaa6af980067efda7d7732d98d50ae65084
naxer-12 <rexan.2212@gmail.com>
2022-10-09 12:28:58 -0400

    changes in next and node file

--- next.config.js
+++ next.config.js
@@ -1,29 +1,49 @@
-// const withMDX = require("@next/mdx")({
-//   extension: /\.mdx?$/,
-// });
-// module.exports = withMDX({
-//   pageExtensions: ["js", "jsx", "md", "mdx"],
-//   i18n: {
-//     locales: ["en-US", "es"],
-//     defaultLocale: "en-US",
+// // const withMDX = require("@next/mdx")({
+// //   extension: /\.mdx?$/,
+// // });
+// // module.exports = withMDX({
+// //   pageExtensions: ["js", "jsx", "md", "mdx"],
+// //   i18n: {
+// //     locales: ["en-US", "es"],
+// //     defaultLocale: "en-US",
+// //   },
+// // });
+// const nextConfig = {
+//   reactStrictMode: true,
+//   // image: {
+//   //   loader: "akamai",
+//   //   path: "https://noop/",
+//   // },
+//   images: {
+//     unoptimized: true,
 //   },
-// });
-const nextConfig = {
-  reactStrictMode: true,
-  // image: {
-  //   loader: "akamai",
-  //   path: "https://noop/",
-  // },
+//   // basePath: "/nextjs-pages",
+//   // assetPrefix: "/nextjs-pages",
+//   exportPathMap: () => ({
+//     "/": {
+//       page: "/",
+//     },
+//   }),
+//   // future: { webpack5: true },
+// };
+// module.exports = nextConfig;
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
-    unoptimized: true,
-  },
-  // basePath: "/nextjs-pages",
-  // assetPrefix: "/nextjs-pages",
-  exportPathMap: () => ({
-    "/": {
-      page: "/",
-    },
-  }),
-  // future: { webpack5: true },
-};
-module.exports = nextConfig;
+        unoptimized: true,
+      },
+}
